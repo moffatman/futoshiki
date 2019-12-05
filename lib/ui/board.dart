0f420cd44cda8c13a1d64cc424cd3df79029558c
Callum Moffat <smartercallum@gmail.com>
2019-12-05 17:14:37 -0500

    Remove overscroll glow on board

--- lib/ui/board.dart
+++ lib/ui/board.dart
@@ -5,6 +5,13 @@ import '../game/board.dart';
 import 'constraint.dart';
 import 'tile.dart';
 
+class _NoOverscrollGlowScrollBehavior extends ScrollBehavior {
+	@override
+	Widget buildViewportChrome(BuildContext context, Widget child, AxisDirection axisDirection) {
+		return child;
+	}
+}
+
 class GameBoardUI extends StatelessWidget {
 	final GameBoard board;
 	final int widgetSize;
@@ -13,33 +20,36 @@ class GameBoardUI extends StatelessWidget {
 
 	@override
 	Widget build(BuildContext context) {
-		return GridView.count(
-			crossAxisCount: widgetSize,
-			shrinkWrap: true,
-			children: List<Widget>.generate(widgetSize * widgetSize, (i) {
-				int layoutRow = i ~/ widgetSize;
-				int layoutColumn = i % widgetSize;
-				int gameRow = layoutRow ~/ 2;
-				int gameColumn = layoutColumn ~/ 2;
-				if (layoutRow % 2 == 0) {
-					// tile row
-					if (layoutColumn % 2 == 0) {
-						return GameTileUI(tile: board.tiles[gameColumn][gameRow], size: board.size);
+		return ScrollConfiguration(
+			behavior: _NoOverscrollGlowScrollBehavior(),
+			child: GridView.count(
+				crossAxisCount: widgetSize,
+				shrinkWrap: true,
+				children: List<Widget>.generate(widgetSize * widgetSize, (i) {
+					int layoutRow = i ~/ widgetSize;
+					int layoutColumn = i % widgetSize;
+					int gameRow = layoutRow ~/ 2;
+					int gameColumn = layoutColumn ~/ 2;
+					if (layoutRow % 2 == 0) {
+						// tile row
+						if (layoutColumn % 2 == 0) {
+							return GameTileUI(tile: board.tiles[gameColumn][gameRow], size: board.size);
+						}
+						else {
+							return (board.horizontalConstraints[gameRow][gameColumn] != null) ? GameHorizontalConstraintUI(board.horizontalConstraints[gameRow][gameColumn]) : Container();
+						}
 					}
 					else {
-						return (board.horizontalConstraints[gameRow][gameColumn] != null) ? GameHorizontalConstraintUI(board.horizontalConstraints[gameRow][gameColumn]) : Container();
+						// constraint row
+						if (layoutColumn % 2 == 0) {
+							return (board.verticalConstraints[gameRow][gameColumn] != null) ? GameVerticalConstraintUI(board.verticalConstraints[gameRow][gameColumn]) : Container();
+						}
+						else {
+							return Container();
+						}
 					}
-				}
-				else {
-					// constraint row
-					if (layoutColumn % 2 == 0) {
-						return (board.verticalConstraints[gameRow][gameColumn] != null) ? GameVerticalConstraintUI(board.verticalConstraints[gameRow][gameColumn]) : Container();
-					}
-					else {
-						return Container();
-					}
-				}
-			})
+				})
+			)
 		);
 	}
 }
